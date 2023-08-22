ec5f2e9df67836c105656ee4b80ecf7e29af5aab
thiago.leite <thiago.leite@dataprev.gov.br>
2023-08-22 08:15:18 -0300

    Reinício do projeto

--- workshop_javax_jdbc/src/application/Main.java
+++ workshop_javax_jdbc/src/application/Main.java
@@ -1,30 +1,25 @@
 package application;
-
-import java.io.IOException;
-
+	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollPane = loader.load();
-			scrollPane.setFitToHeight(true);
-			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
-			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (IOException e) {
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
